8d4546263ea47f037770d9394e066d3f6c15857c
Tornike <itorn9777@gmail.com>
2023-01-09 00:18:15 +0400

    initial commit, created dev enviroment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './css/styles.css'
+import { RAWG_KEY } from '../settings.json';
+
+function App() {
+
+  return (
+    <div className="App">
+      app
+    </div>
+  )
+}
+
+export default App
